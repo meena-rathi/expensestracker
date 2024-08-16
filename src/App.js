025dd5a3a2c65572aaeb1a487b131426302ad230
Meena Kumari <meenakumari16@hotmail.com>
2024-08-16 07:08:31 +0000

    add cteogry form

--- src/App.js
+++ src/App.js
@@ -6,14 +6,18 @@ import SignUpForm from './pages/auth/SignUpForm';
 import Navbar from './components/NavBar';
 import Home from './pages/expenses/Home';
 
+import { useCurrentUser } from "./contexts/CurrentUserContext"
+
 function App() {
+  const currentUser = useCurrentUser();
   return (
     <Router>
       <Navbar />
       <Switch>
         <Route exact path="/signin" component={SignInForm} />
         <Route exact path="/signup" component={SignUpForm} />
-        <Route path="/home" component={Home} />
+        {/* <Route exact path="/home" render={()=><Home/>}/> */}
+        <Route exact path="/home" component={Home} />
         {/* Add other routes here */}
       </Switch>
     </Router>
