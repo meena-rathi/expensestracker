a0d218a67d4ebd3f1dcced8a7084c736bd3d162c
Meena Kumari <meenakumari16@hotmail.com>
2024-08-28 13:31:50 +0000

    add the pagination for the expenses

--- src/components/ExpensesDisplay.js
+++ src/components/ExpensesDisplay.js
@@ -1,13 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styles from '../styles/ExpensesList.module.css';
-
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faChevronLeft, faChevronRight } from '@fortawesome/free-solid-svg-icons';
 const ExpensesDisplay = ({ expenses }) => {
-  return (
+  const [currentPage, setCurrentPage] = useState(1);
+  const itemsPerpage = 10;
+
+  const indexOfLastItem = currentPage * itemsPerpage;
+  const indexOfFirstItem = indexOfLastItem - itemsPerpage;
+  const currentExpenses = expenses.slice(indexOfFirstItem, indexOfLastItem);
+
+  const totalPages = Math.ceil(expenses.length/itemsPerpage) 
+  const nextPage = () => {
+    if (currentPage < totalPages) {
+      setCurrentPage(currentPage + 1);
+    }
+  };
+
+  const prevPage = () => {
+    if (currentPage > 1) {
+      setCurrentPage(currentPage - 1);
+    }
+  };
+   return (
     <div>
       <h2>Expenses List</h2>
       {expenses.length > 0 ? (
         <table className={styles.expenses}>
-          {expenses.map((expense) => (
+          {currentExpenses.map((expense) => (
             <tr key={expense.id}>
               <td> ${expense.amount}</td>
               <td>{expense.description}</td>
@@ -19,6 +39,17 @@ const ExpensesDisplay = ({ expenses }) => {
       ) : (
         <p>No expenses found.</p>
       )}
+       <div className={styles.pagination}>
+        <button onClick={prevPage} disabled={currentPage === 1}>
+          <FontAwesomeIcon icon={faChevronLeft} /> Previous
+        </button>
+        <span>
+          Page {currentPage} of {totalPages}
+        </span>
+        <button onClick={nextPage} disabled={currentPage === totalPages}>
+          Next <FontAwesomeIcon icon={faChevronRight} />
+        </button>
+      </div>
     </div>
   );
 };
