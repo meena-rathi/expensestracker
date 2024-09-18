e50e7332f23ab275aa54a462bbafe04c5204dfb8
Meena Kumari <meenakumari16@hotmail.com>
2024-09-18 10:18:31 +0000

    fix icon

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -5,9 +5,7 @@ import pluginReact from "eslint-plugin-react";
 
 export default [
   {files: ["**/*.{js,mjs,cjs,jsx}"]},
-  {files: ["**/*.js"], languageOptions: {sourceType: "commonjs"}},
   {languageOptions: { globals: globals.browser }},
   pluginJs.configs.recommended,
   pluginReact.configs.flat.recommended,
 ];
-
--- src/components/TotalExpenses.js
+++ src/components/TotalExpenses.js
@@ -1,24 +1,3 @@
-// import React from "react";
-// import styles from "../styles/Home.module.css";
-
-// const TotalExpenses = ({ budget = 0, expenses = [] }) => {
-
-//   const totalExpenses = expenses.reduce((total, expense) => {
-//     return total + parseFloat(expense.amount || 0);
-//   }, 0);
-
-//   const remainingBudget = budget - totalExpenses;
-
-//   return (
-//     <div>
-//       <p className={styles.expensesDisplay}>Total Expenses: ${totalExpenses.toFixed(2)}</p>
-//       <p className={styles.expensesDisplay}>Remaining Budget: ${remainingBudget.toFixed(2)}</p>
-//     </div>
-//   );
-// };
-
-// export default TotalExpenses;
-
 import React from "react";
 import styles from "../styles/total.module.css";
 
